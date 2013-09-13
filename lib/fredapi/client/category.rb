5c3c1bae69742f093e043c5030ca21ffd2bb855c
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2013-09-13 23:50:38 +0100

    Initial commit of category/children endpoint

--- lib/fredapi/client/category.rb
+++ lib/fredapi/client/category.rb
@@ -1,8 +1,15 @@
+require 'fredapi/client/category/children'
+
 module FREDAPI
   class Client
+    # Category module for category endpoint
     module Category
 
+      def category opts={}
+        get "fred/category", opts
+      end
 
+      include FREDAPI::Client::Category::Children
 
     end
   end
